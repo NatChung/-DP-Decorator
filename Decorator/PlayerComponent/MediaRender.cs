ef62a427e7da5478cdf9b988570709588b8ba44b
Nat <nat.chung1@gmail.com>
2017-04-19 11:02:53 +0800

    Initial commit

---
+++ Decorator/PlayerComponent/MediaRender.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decorator.PlayerComponent
+{
+	public class MediaRender
+	{
+		public View View { get; set; }
+		internal void AddVideoData(byte[] v)
+		{
+			Console.WriteLine("Added video data");
+		}
+
+		internal void AddAudioData(byte[] v)
+		{
+			Console.WriteLine("Added audio data");
+		}
+	}
+}
---
+++ Decorator/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decorator
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			SimplePlayer player = new SimplePlayer("http://127.0.0.1/source");
+			player.Play();
+
+			Console.ReadLine();
+		}
+	}
+}
