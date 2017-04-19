ed3f11f28c842eff3354352555ba6b1dbdfedaad
Nat <nat.chung1@gmail.com>
2017-04-19 15:26:11 +0800

    [A]Added Resizeable test with error

--- DecoratorTests/SimplePlayerTest.cs
+++ DecoratorTests/SimplePlayerTest.cs
@@ -9,6 +9,7 @@ namespace DecoratorTests
 	public class SimplePlayerTest
 	{
 		[Fact]
+		[Trait("Decorator", "Simple player")]
 		public void PlayTest()
 		{
 			var expected = Substitute.For<IMediaRender>();
@@ -21,6 +22,22 @@ namespace DecoratorTests
 			var view = expected.Received().View;
 			Thread.Sleep(100);
 
+		}
+
+		[Fact]
+		[Trait("Decorator", "Resizable player")]
+		public void ResizeablePlayerTest()
+		{
+			var expected = Substitute.For<IMediaRender>();
+			ResizeableRender resizeableRender = new ResizeableRender(expected);
+
+			ResizeablePlayerTest = new ResizeablePlayer(resizeableRender);
+			sut.Play("http://xxx.xxx");
+
+			expected.ReceivedWithAnyArgs().AddVideoData(default(byte[]));
+			expected.ReceivedWithAnyArgs().AddAudioData(default(byte[]));
+			var view = expected.Received().View;
+			Thread.Sleep(100);
 		}
 	}
 }
