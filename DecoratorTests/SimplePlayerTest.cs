afae6d16050bed67f6a5e5b11573c10e9b74bbc3
Nat <nat.chung1@gmail.com>
2017-04-19 12:24:41 +0800

    [M]Added simple player unit test with compiler error

--- DecoratorTests/SimplePlayerTest.cs
+++ DecoratorTests/SimplePlayerTest.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Decorator;
 using Xunit;
+using NSubstitute;
 
 namespace DecoratorTests
 {
@@ -12,7 +9,14 @@ namespace DecoratorTests
 		[Fact]
 		public void PlayTest()
 		{
-			
+			var expected = Substitute.For<IMediaRender>();
+			SimplePlayer sut = new SimplePlayer(expected);
+
+			sut.Play();
+
+			expected.ReceivedWithAnyArgs(1).AddVideoData(default(byte[]));
+			expected.RecvivedWithAnyArgs(1).AddAudioData(default(byte[]));
+
 		}
 	}
 }
