eb376004b37bf50eb52d2afa22e705c1be224a19
Nat <nat.chung1@gmail.com>
2017-04-19 11:10:29 +0800

    [A]Added Xunit and DecoratorTests

---
+++ DecoratorTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DecoratorTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DecoratorTests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4136d583-38fa-45e6-bdce-9a9946eb2e54")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ DecoratorTests/SimplePlayerTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DecoratorTests
+{
+	public class SimplePlayerTest
+	{
+		[Fact]
+		public void PlayTest()
+		{
+			
+		}
+	}
+}
