4d40166d8585a56cabfedd620d800b4bf4bb4f2f
Nat <nat.chung1@gmail.com>
2017-04-20 12:28:13 +0800

    [M]Meida source decorator test passed

---
+++ Decorator/PlayerComponent/JitterBuffer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decorator.PlayerComponent
+{
+	public class JitterBuffer : MediaSourceDecorator
+	{
+		public JitterBuffer(IMediaSource source) : base(source)
+		{
+		}
+	}
+}
---
+++ Decorator/PlayerComponent/MediaSourceDecorator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Decorator.PlayerComponent
+{
+	public class MediaSourceDecorator : IMediaSource
+	{
+		private IMediaSource mediaSource { get; set; }
+		public Action<byte[]> OnVideoData { get; set; }
+		public Action<byte[]> OnAudioData { get; set; }
+
+		public MediaSourceDecorator(IMediaSource source)
+		{
+			this.mediaSource = source;
+			OnVideoData = this.mediaSource.OnVideoData;
+			OnAudioData = this.mediaSource.OnAudioData;
+		}
+
+		public virtual void AddVideoData(byte[] data)
+		{
+			this.mediaSource.AddVideoData(data);
+		}
+
+		public virtual void AddAudioData(byte[] data)
+		{
+			this.mediaSource.AddAudioData(data);
+		}
+	}
+}
