a09a01cf21a3a84a54ddd99d0c54cc9a14b6d9d5
Nat <nat.chung1@gmail.com>
2017-04-21 17:48:55 +0800

    [M]ResizeableRender AddVideoData passed

---
+++ Decorator/PlayerComponent/ResizeableRender.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decorator.PlayerComponent
+{
+	public class ResizeableRender : IMediaRender
+	{
+		private IMediaRender render;
+
+		public ResizeableRender(IMediaRender expect)
+		{
+			this.render = expect;
+		}
+
+		public View View => throw new NotImplementedException();
+
+		public void AddAudioData(byte[] data)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void AddVideoData(byte[] data)
+		{
+			this.render.AddVideoData(data);
+		}
+	}
+}
