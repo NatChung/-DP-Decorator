ca637f0e6cd4c7595a276630fb3310959b359390
Nat <nat.chung1@gmail.com>
2017-04-21 17:16:57 +0800

    [A]Added player test code with error

--- DecoratorTests/SimplePlayerTest.cs
+++ DecoratorTests/SimplePlayerTest.cs
@@ -3,12 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NSubstitute;
 using Xunit;
 
 namespace DecoratorTests
 {
 	public class SimplePlayerTest
 	{
-		
+		[Fact]
+		[Trait("Player", "Decorator")]
+		public void PlayerTest()
+		{
+			var expect = Substitute.For<IMediaRender>();
+
+			var sut = new Player(expect);
+			sut.Play();
+
+			expect.ReceivedWithAnyArgs().AddVideoData(default(byte[]);
+			expect.ReceivedWithAnyArgs().AddAudioData(default(byte[]);
+		}
 	}
 }
